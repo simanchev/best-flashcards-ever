cab632b036ec10a058ca9bba4aae82f6bb5f7b6f
Alex <alexander.simanchev@gmail.com>
2022-07-21 16:50:19 +0300

    bootstrap introduced

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -5,7 +5,9 @@ function Layout({ title, children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/stylesheets/style.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
         <script defer src="/js/script.js" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         <title>{title}</title>
       </head>
       <body>
