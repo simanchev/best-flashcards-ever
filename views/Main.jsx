b6c8ce21b1530d8b05fab28431d8a6f15fbdae90
Alex <alexander.simanchev@gmail.com>
2022-07-21 20:14:41 +0300

    main added

--- views/Main.jsx
+++ views/Main.jsx
@@ -1,51 +1,22 @@
 const React = require('react');
 const Layout = require('./Layout');
+const TopicCard = require('./TopicCard');
+const Nav = require('./Nav');
 
 function Main({ title }) {
   return (
     <Layout title={title}>
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <h1>Flashcards</h1>
-          <ul className="nav justify-content-end">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">register</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">login</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">home</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Nav />
       <div className="container-fluid">
-        <h3>Темы вопросов</h3>
+        <h3 style={{ textAlign: 'center' }}>Тему выбирай</h3>
       </div>
       <div className="container-fluid">
-        <div className="card" style={{ width: '18rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div className="card" style={{ width: '18rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div className="card" style={{ width: '18rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
+        <ul className="topics-list">
+          <TopicCard />
+          <TopicCard />
+          <TopicCard />
+          <TopicCard />
+        </ul>
       </div>
     </Layout>
   );
---
+++ views/TopicCard.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+function TopicCard({ id }) {
+  return (
+    <li className="card" id={id} style={{ width: '18rem' }}>
+      <img src="/src/demo-img.png" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </li>
+  );
+}
+
+module.exports = TopicCard;
