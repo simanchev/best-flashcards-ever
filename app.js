eaf57825ea97091d78caaa96380c4768003299c3
Alex <alexander.simanchev@gmail.com>
2022-07-22 10:59:12 +0300

    quest.jsx

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/', mainRouter);
 app.use('/quest', questRouter);
 app.use(express.static(path.join(__dirname, 'public')));
--- views/Quest.jsx
+++ views/Quest.jsx
@@ -2,21 +2,26 @@ const React = require('react');
 const Layout = require('./Layout');
 const Nav = require('./Nav');
 
-function Quest({ question, answer_1, answer_2, answer_correct }) {
+function Quest({ check, question, answer_1, answer_2, answer_correct }) {
   const arrAnsw = [answer_1, answer_2, answer_correct];
   return (
     <Layout>
       <Nav />
-      <div className="container-fluid">
-        <h3 className="question">{question}</h3>
-        <ul class="list-group">
-          {arrAnsw.map((answer) => (
-            <li class="list-group-item">
-              <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio" />
-              <label class="form-check-label" for="firstRadio">{answer}</label>
-            </li>))}
-        </ul>
-      </div>
+      <form action="/quest" method="post">
+        <div className="container-fluid">
+          <h5 className="check">{check}</h5>
+          <h3 className="question" name="question">{question}</h3>
+          <ul className="list-group">
+            {arrAnsw.map((answer) => (
+              <li class="list-group-item">
+                <input class="form-check-input me-1" type="radio" name="answer" value={answer} id="firstRadio" />
+                <label class="form-check-label" for="firstRadio">{answer}</label>
+              </li>))}
+          </ul>
+          <input type="text" className="hidden-input" name="question" value={question} />
+          <button type="submit" className="btn btn-primary">Ответить</button>
+        </div>
+      </form>
     </Layout>
   );
 }
